ab7e148301b61e522b02d85bb369008710b92cb7
Hussein Dawod <56936130+husseindawod@users.noreply.github.com>
2024-12-24 01:09:13 -0500

    .

--- 1_Web_Scraping/webscraping.py
+++ 1_Web_Scraping/webscraping.py
@@ -3,3 +3,7 @@ from bs4 import BeautifulSoup as bs
 
 github_user = input("Input GitHub User: ")
 url = 'https://github.com/' + github_user
+r = requests.get(url)
+soup = bs(r.content, 'html.parser')
+profile_img = soup.find('img', {'class': 'avatar-user'})['src']
+print(profile_img)
